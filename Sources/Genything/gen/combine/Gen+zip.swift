e663bdf4c7caf9340d16159490f3709d62c41b91
Nico Richard <nicolas.richard@skipthedishes.ca>
2021-11-17 10:03:19 -0600

    Add some more zip arities

--- Sources/Genything/gen/combine/Gen+zip.swift
+++ Sources/Genything/gen/combine/Gen+zip.swift
@@ -226,7 +226,7 @@ extension Gen {
     ///    - gen3: Third generator
     ///    - gen4: Fourth generator
     ///    - gen5: Fifth generator
-    ///    - gen5: Sixth generator
+    ///    - gen6: Sixth generator
     ///
     /// - Returns: A generator of values
     public static func zip<T1, T2, T3, T4, T5, T6>(
@@ -259,7 +259,7 @@ extension Gen {
     ///    - gen3: Third generator
     ///    - gen4: Fourth generator
     ///    - gen5: Fifth generator
-    ///    - gen5: Sixth generator
+    ///    - gen6: Sixth generator
     ///    - transform: A function capable of transforming the values produced by the resulting zipped generator
     ///
     /// - Returns: A generator of values
@@ -275,5 +275,315 @@ extension Gen {
         Gen<(T1, T2, T3, T4, T5, T6)>
             .zip(gen1, gen2, gen3, gen4, gen5, gen6)
             .map({ t in transform(t.0, t.1, t.2, t.3, t.4, t.5) })
+    }
+}
+
+// - MARK: zip7
+
+extension Gen {
+    /// Returns: A generator by zipping together the values produced by the supplied generators
+    ///
+    /// - Note: This is a very complex zip operation. Perhaps you should consider using `Gen.compose` instead!
+    ///
+    /// - Parameters:
+    ///    - gen1: First generator
+    ///    - gen2: Second generator
+    ///    - gen3: Third generator
+    ///    - gen4: Fourth generator
+    ///    - gen5: Fifth generator
+    ///    - gen6: Sixth generator
+    ///    - gen7: Seventh generator
+    ///
+    /// - Returns: A generator of values
+    public static func zip<T1, T2, T3, T4, T5, T6, T7>(
+        _ gen1: Gen<T1>,
+        _ gen2: Gen<T2>,
+        _ gen3: Gen<T3>,
+        _ gen4: Gen<T4>,
+        _ gen5: Gen<T5>,
+        _ gen6: Gen<T6>,
+        _ gen7: Gen<T7>
+    ) -> Gen<(T1, T2, T3, T4, T5, T6, T7)> where T == (T1, T2, T3, T4, T5, T6, T7) {
+        Gen.compose {
+            (
+                $0.generate(gen1),
+                $0.generate(gen2),
+                $0.generate(gen3),
+                $0.generate(gen4),
+                $0.generate(gen5),
+                $0.generate(gen6),
+                $0.generate(gen7)
+            )
+        }
+    }
+
+    /// Returns: A generator by zipping together the values produced by the supplied generators
+    ///
+    /// - Note: This is a very complex zip operation. Perhaps you should consider using `Gen.compose` instead!
+    ///
+    /// - Parameters:
+    ///    - gen1: First generator
+    ///    - gen2: Second generator
+    ///    - gen3: Third generator
+    ///    - gen4: Fourth generator
+    ///    - gen5: Fifth generator
+    ///    - gen5: Sixth generator
+    ///    - gen7: Seventh generator
+    ///    - transform: A function capable of transforming the values produced by the resulting zipped generator
+    ///
+    /// - Returns: A generator of values
+    public static func zip<T1, T2, T3, T4, T5, T6, T7>(
+        _ gen1: Gen<T1>,
+        _ gen2: Gen<T2>,
+        _ gen3: Gen<T3>,
+        _ gen4: Gen<T4>,
+        _ gen5: Gen<T5>,
+        _ gen6: Gen<T6>,
+        _ gen7: Gen<T7>,
+        transform: @escaping (T1, T2, T3, T4, T5, T6, T7) -> T
+    ) -> Gen<T> {
+        Gen<(T1, T2, T3, T4, T5, T6, T7)>
+            .zip(gen1, gen2, gen3, gen4, gen5, gen6, gen7)
+            .map({ t in transform(t.0, t.1, t.2, t.3, t.4, t.5, t.6) })
+    }
+}
+
+// - MARK: zip8
+
+extension Gen {
+    /// Returns: A generator by zipping together the values produced by the supplied generators
+    ///
+    /// - Note: This is a very complex zip operation. Perhaps you should consider using `Gen.compose` instead!
+    ///
+    /// - Parameters:
+    ///    - gen1: First generator
+    ///    - gen2: Second generator
+    ///    - gen3: Third generator
+    ///    - gen4: Fourth generator
+    ///    - gen5: Fifth generator
+    ///    - gen6: Sixth generator
+    ///    - gen7: Seventh generator
+    ///    - gen8: Eighth generator
+    ///
+    /// - Returns: A generator of values
+    public static func zip<T1, T2, T3, T4, T5, T6, T7, T8>(
+        _ gen1: Gen<T1>,
+        _ gen2: Gen<T2>,
+        _ gen3: Gen<T3>,
+        _ gen4: Gen<T4>,
+        _ gen5: Gen<T5>,
+        _ gen6: Gen<T6>,
+        _ gen7: Gen<T7>,
+        _ gen8: Gen<T8>
+    ) -> Gen<(T1, T2, T3, T4, T5, T6, T7, T8)> where T == (T1, T2, T3, T4, T5, T6, T7, T8) {
+        Gen.compose {
+            (
+                $0.generate(gen1),
+                $0.generate(gen2),
+                $0.generate(gen3),
+                $0.generate(gen4),
+                $0.generate(gen5),
+                $0.generate(gen6),
+                $0.generate(gen7),
+                $0.generate(gen8)
+            )
+        }
+    }
+
+    /// Returns: A generator by zipping together the values produced by the supplied generators
+    ///
+    /// - Note: This is a very complex zip operation. Perhaps you should consider using `Gen.compose` instead!
+    ///
+    /// - Parameters:
+    ///    - gen1: First generator
+    ///    - gen2: Second generator
+    ///    - gen3: Third generator
+    ///    - gen4: Fourth generator
+    ///    - gen5: Fifth generator
+    ///    - gen5: Sixth generator
+    ///    - gen7: Seventh generator
+    ///    - gen8: Eighth generator
+    ///    - transform: A function capable of transforming the values produced by the resulting zipped generator
+    ///
+    /// - Returns: A generator of values
+    public static func zip<T1, T2, T3, T4, T5, T6, T7, T8>(
+        _ gen1: Gen<T1>,
+        _ gen2: Gen<T2>,
+        _ gen3: Gen<T3>,
+        _ gen4: Gen<T4>,
+        _ gen5: Gen<T5>,
+        _ gen6: Gen<T6>,
+        _ gen7: Gen<T7>,
+        _ gen8: Gen<T8>,
+        transform: @escaping (T1, T2, T3, T4, T5, T6, T7, T8) -> T
+    ) -> Gen<T> {
+        Gen<(T1, T2, T3, T4, T5, T6, T7, T8)>
+            .zip(gen1, gen2, gen3, gen4, gen5, gen6, gen7, gen8)
+            .map({ t in transform(t.0, t.1, t.2, t.3, t.4, t.5, t.6, t.7) })
+    }
+}
+
+// - MARK: zip9
+
+extension Gen {
+    /// Returns: A generator by zipping together the values produced by the supplied generators
+    ///
+    /// - Note: This is a very complex zip operation. Perhaps you should consider using `Gen.compose` instead!
+    ///
+    /// - Parameters:
+    ///    - gen1: First generator
+    ///    - gen2: Second generator
+    ///    - gen3: Third generator
+    ///    - gen4: Fourth generator
+    ///    - gen5: Fifth generator
+    ///    - gen6: Sixth generator
+    ///    - gen7: Seventh generator
+    ///    - gen8: Eighth generator
+    ///    - gen9: Ninth generator
+    ///
+    /// - Returns: A generator of values
+    public static func zip<T1, T2, T3, T4, T5, T6, T7, T8, T9>(
+        _ gen1: Gen<T1>,
+        _ gen2: Gen<T2>,
+        _ gen3: Gen<T3>,
+        _ gen4: Gen<T4>,
+        _ gen5: Gen<T5>,
+        _ gen6: Gen<T6>,
+        _ gen7: Gen<T7>,
+        _ gen8: Gen<T8>,
+        _ gen9: Gen<T9>
+    ) -> Gen<(T1, T2, T3, T4, T5, T6, T7, T8, T9)> where T == (T1, T2, T3, T4, T5, T6, T7, T8, T9) {
+        Gen.compose {
+            (
+                $0.generate(gen1),
+                $0.generate(gen2),
+                $0.generate(gen3),
+                $0.generate(gen4),
+                $0.generate(gen5),
+                $0.generate(gen6),
+                $0.generate(gen7),
+                $0.generate(gen8),
+                $0.generate(gen9)
+            )
+        }
+    }
+
+    /// Returns: A generator by zipping together the values produced by the supplied generators
+    ///
+    /// - Note: This is a very complex zip operation. Perhaps you should consider using `Gen.compose` instead!
+    ///
+    /// - Parameters:
+    ///    - gen1: First generator
+    ///    - gen2: Second generator
+    ///    - gen3: Third generator
+    ///    - gen4: Fourth generator
+    ///    - gen5: Fifth generator
+    ///    - gen5: Sixth generator
+    ///    - gen7: Seventh generator
+    ///    - gen8: Eighth generator
+    ///    - gen9: Ninth generator
+    ///    - transform: A function capable of transforming the values produced by the resulting zipped generator
+    ///
+    /// - Returns: A generator of values
+    public static func zip<T1, T2, T3, T4, T5, T6, T7, T8, T9>(
+        _ gen1: Gen<T1>,
+        _ gen2: Gen<T2>,
+        _ gen3: Gen<T3>,
+        _ gen4: Gen<T4>,
+        _ gen5: Gen<T5>,
+        _ gen6: Gen<T6>,
+        _ gen7: Gen<T7>,
+        _ gen8: Gen<T8>,
+        _ gen9: Gen<T9>,
+        transform: @escaping (T1, T2, T3, T4, T5, T6, T7, T8, T9) -> T
+    ) -> Gen<T> {
+        Gen<(T1, T2, T3, T4, T5, T6, T7, T8, T9)>
+            .zip(gen1, gen2, gen3, gen4, gen5, gen6, gen7, gen8, gen9)
+            .map({ t in transform(t.0, t.1, t.2, t.3, t.4, t.5, t.6, t.7, t.8) })
+    }
+}
+
+// - MARK: zip10
+
+extension Gen {
+    /// Returns: A generator by zipping together the values produced by the supplied generators
+    ///
+    /// - Note: This is a very complex zip operation. Perhaps you should consider using `Gen.compose` instead!
+    ///
+    /// - Parameters:
+    ///    - gen1: First generator
+    ///    - gen2: Second generator
+    ///    - gen3: Third generator
+    ///    - gen4: Fourth generator
+    ///    - gen5: Fifth generator
+    ///    - gen6: Sixth generator
+    ///    - gen7: Seventh generator
+    ///    - gen8: Eighth generator
+    ///    - gen9: Ninth generator
+    ///    - gen10: Tenth generator
+    ///
+    /// - Returns: A generator of values
+    public static func zip<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10>(
+        _ gen1: Gen<T1>,
+        _ gen2: Gen<T2>,
+        _ gen3: Gen<T3>,
+        _ gen4: Gen<T4>,
+        _ gen5: Gen<T5>,
+        _ gen6: Gen<T6>,
+        _ gen7: Gen<T7>,
+        _ gen8: Gen<T8>,
+        _ gen9: Gen<T9>,
+        _ gen10: Gen<T10>
+    ) -> Gen<(T1, T2, T3, T4, T5, T6, T7, T8, T9, T10)> where T == (T1, T2, T3, T4, T5, T6, T7, T8, T9, T10) {
+        Gen.compose {
+            (
+                $0.generate(gen1),
+                $0.generate(gen2),
+                $0.generate(gen3),
+                $0.generate(gen4),
+                $0.generate(gen5),
+                $0.generate(gen6),
+                $0.generate(gen7),
+                $0.generate(gen8),
+                $0.generate(gen9),
+                $0.generate(gen10)
+            )
+        }
+    }
+
+    /// Returns: A generator by zipping together the values produced by the supplied generators
+    ///
+    /// - Note: This is a very complex zip operation. Perhaps you should consider using `Gen.compose` instead!
+    ///
+    /// - Parameters:
+    ///    - gen1: First generator
+    ///    - gen2: Second generator
+    ///    - gen3: Third generator
+    ///    - gen4: Fourth generator
+    ///    - gen5: Fifth generator
+    ///    - gen5: Sixth generator
+    ///    - gen7: Seventh generator
+    ///    - gen8: Eighth generator
+    ///    - gen9: Ninth generator
+    ///    - gen10: Tenth generator
+    ///    - transform: A function capable of transforming the values produced by the resulting zipped generator
+    ///
+    /// - Returns: A generator of values
+    public static func zip<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10>(
+        _ gen1: Gen<T1>,
+        _ gen2: Gen<T2>,
+        _ gen3: Gen<T3>,
+        _ gen4: Gen<T4>,
+        _ gen5: Gen<T5>,
+        _ gen6: Gen<T6>,
+        _ gen7: Gen<T7>,
+        _ gen8: Gen<T8>,
+        _ gen9: Gen<T9>,
+        _ gen10: Gen<T10>,
+        transform: @escaping (T1, T2, T3, T4, T5, T6, T7, T8, T9, T10) -> T
+    ) -> Gen<T> {
+        Gen<(T1, T2, T3, T4, T5, T6, T7, T8, T9, T10)>
+            .zip(gen1, gen2, gen3, gen4, gen5, gen6, gen7, gen8, gen9, gen10)
+            .map({ t in transform(t.0, t.1, t.2, t.3, t.4, t.5, t.6, t.7, t.8, t.9) })
     }
 }
