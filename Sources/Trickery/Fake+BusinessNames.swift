76e9a48b8a7bcba352c8493c22acb1352530c293
Magalie Tatischeff <magalie.tatischeff@justeattakeaway.com>
2021-11-16 18:11:58 -0800

    test for BusinessNames

--- Sources/Trickery/Fake+BusinessNames.swift
+++ Sources/Trickery/Fake+BusinessNames.swift
@@ -5,10 +5,11 @@ extension Fake {
     public enum BusinessNames {
         private static let data: BusinessNamesData = BusinessNamesData.loadJson()
         public static let restaurant: Gen<String> = .zipWith(Fake.PersonNames.first, Gen<String>.of(data.restaurantEndings)) { "\($0)'s \($1)" }
-        public static let general: Gen<String> = .one(of: [ .zipWith(Fake.PersonNames.first, Gen<String>.of(data.generalEndings)) { "\($0)'s \($1)" },
-                                                             .zipWith(Fake.PersonNames.first, Gen<String>.of(data.abreviations)) {  "\($0) \($1)" } ])
-        public static let any: Gen<String> = .one(of: [ restaurant,
-                                                        general ])
+        public static let general: Gen<String> = .one(of: [
+            .zipWith(Fake.PersonNames.first, Gen<String>.of(data.generalEndings)) { "\($0)'s \($1)" },
+            .zipWith(Fake.PersonNames.first, Gen<String>.of(data.abreviations)) {  "\($0) \($1)" }
+        ])
+        public static let any: Gen<String> = .one(of: [ restaurant, general ])
     }
 }
 
