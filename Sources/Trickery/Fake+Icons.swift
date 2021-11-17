e70696cb3eba57fdf7acaeaf74db6e40896cb621
Magalie Tatischeff <magalie.tatischeff@justeattakeaway.com>
2021-11-17 11:04:12 -0800

    Tests for Fake.Icons

--- Sources/Trickery/Fake+Icons.swift
+++ Sources/Trickery/Fake+Icons.swift
@@ -1,13 +1,10 @@
 import Foundation
 import Genything
 import SFSafeSymbols
-import SwiftUI
 
 extension Fake {
     public enum Icons {
         @available(iOS 13.0, macOS 10.15, tvOS 13.0, watchOS 6.0, *)
         public static let sfSymbols: Gen<SFSymbol> = .ofCases()
-        @available(iOS 13.0, macOS 10.15, tvOS 13.0, watchOS 6.0, *)
-        public static let sfSymbolImages: Gen<Image> = sfSymbols.map { Image(systemSymbol: $0) }
     }
 }
