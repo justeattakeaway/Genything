faaaf90a21a3c9ee5a6404e55248fd26e8bbaa71
Magalie Tatischeff <magalie.tatischeff@justeattakeaway.com>
2021-11-16 18:29:09 -0800

    Refactored Names test

--- Tests/TrickeryTests/FakeNamesTest.swift
+++ Tests/TrickeryTests/FakeNamesTest.swift
@@ -2,31 +2,20 @@ import XCTest
 @testable import Trickery
 
 class FakeNamesTests: XCTestCase {
-    func test_names_first() {
-        Fake.Names.first.take().forEach {
-            XCTAssertFalse($0.isEmpty)
-        }
-    }
-    
-    func test_names_middle() {
-        Fake.Names.middle.take().forEach { names in
-            let numberOfNames = names.components(separatedBy: .whitespaces).count
-            // Some names like "Jo Ann" can have 2 words
-            XCTAssertTrue( numberOfNames < 3 )
-        }
-    }
-    
-    func test_names_last() {
-        Fake.Names.last.take().forEach {
-            XCTAssertFalse($0.isEmpty)
-        }
-    }
-    
     func test_names_full() {
-        Fake.Names.full.take().forEach { names in
-            let numberOfNames = names.components(separatedBy: .whitespaces).count
-            XCTAssertTrue( numberOfNames >= 2 && numberOfNames <= 4 )
+        Fake.Names.full.take().forEach {
+            let names = $0.components(separatedBy: .whitespaces)
+            names.forEach {
+                XCTAssertEqual(true, $0.first?.isUppercase)
+            }
         }
     }
     
+    func test_names_full_number_of_words() {
+        Fake.Names.full.take().forEach {
+            // Some names like "Jo Ann" can have 2 words
+            let numberOfWords = $0.components(separatedBy: .whitespaces).count
+            XCTAssertTrue( numberOfWords >= 2 && numberOfWords <= 4 )
+        }
+    }
 }
