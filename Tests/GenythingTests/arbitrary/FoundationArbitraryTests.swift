0739c155e01d5a5723b0b3becb41912f1e9d67f5
Nico Richard <nicorichard@gmail.com>
2022-01-14 13:53:02 -0600

    fix test

--- Tests/GenythingTests/arbitrary/FoundationArbitraryTests.swift
+++ Tests/GenythingTests/arbitrary/FoundationArbitraryTests.swift
@@ -10,25 +10,9 @@ final internal class FoundationArbitraryTests: XCTestCase {
 
     func test_dateAbitraryCanGenerateRandomDates() {
         var datesSet: Set<Date> = Set()
-        Date.arbitrary.take(count: arbitraryCount).forEach {
+        Date.arbitrary.unique().take(count: arbitraryCount).forEach {
             datesSet.insert($0)
         }
         XCTAssertEqual(datesSet.count, arbitraryCount)
     }
-
-    //    func test_complexUrlString() {
-    //        _ = URL.arbitrary.sample() // Checking for fatalError
-    //    }
-    //
-    //    func test_forAll() {
-    //        let count = 100
-    //
-    //        var i = 0
-    //        URL.arbitrary.take(count: count).forEach {
-    //            XCTAssert(!$0.isFileURL)
-    //            i += 1
-    //        }
-    //
-    //        XCTAssertEqual(i, count)
-    //    }
 }
