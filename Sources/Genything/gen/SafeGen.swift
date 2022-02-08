81a79ad8e8a4bb18012bece2ee61115e0351eedc
Nico Richard <nicorichard@gmail.com>
2022-02-08 12:22:54 -0600

    improve docs

--- Sources/Genything/gen/SafeGen.swift
+++ Sources/Genything/gen/SafeGen.swift
@@ -1,25 +1,25 @@
 import Foundation
 
-/// Combines Gen member lookup with the "safe" extensions
+/// Wrapper to namespace safe (throwing) extension functions for `Gen`
 public struct SafeGen<T> {
-    /// The base Gen instance which has been enhanced by `safe` functionality
-    let base: Gen<T>
-    /// Creates extensions with base object.
-    ///
-    /// - parameter base: Base object.
-    init(_ base: Gen<T>) {
+    /// The base Gen instance
+    fileprivate let base: Gen<T>
+
+    /// Initializes a `SafeGen` wrapping `base`
+    fileprivate init(_ base: Gen<T>) {
         self.base = base
     }
 }
 
 public extension SafeGen {
+    /// Reverts back to a standard `Gen`
     var unsafe: Gen<T> {
         base
     }
 }
 
 public extension Gen {
-    /// Namespacing for safe extension functions for `Gen`
+    /// Converts the `Gen` to the `SafeGen` wrapper
     var safe: SafeGen<T> {
         SafeGen(self)
     }
