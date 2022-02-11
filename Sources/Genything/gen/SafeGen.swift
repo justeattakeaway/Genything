e5520e94c1f5c4a2ea7ac1726d2834703e38b4b2
Nico Richard <nicorichard@gmail.com>
2022-02-11 14:29:08 -0600

    Safe gen improvements (#30)
    * Remove unnecessary and flawed dynamic member lookup for safe
    
    * improve docs
    
    * Revert making properties private

--- Sources/Genything/gen/SafeGen.swift
+++ Sources/Genything/gen/SafeGen.swift
@@ -1,30 +1,26 @@
 import Foundation
 
-/// Combines Gen member lookup with the "safe" extensions
-@dynamicMemberLookup
+/// Wrapper to namespace safe (throwing) extension functions for `Gen`
 public struct SafeGen<T> {
-    /// The base Gen instance which has been enhanced by `safe` functionality
+    /// The base Gen instance
     let base: Gen<T>
-    /// Creates extensions with base object.
-    ///
-    /// - parameter base: Base object.
+
+    /// Initializes a `SafeGen` wrapping `base`
     init(_ base: Gen<T>) {
         self.base = base
     }
+}
 
-    /// Enhances `SafeGen` to include the members of the base `Gen`
-    subscript<Property>(dynamicMember keyPath: KeyPath<Gen<T>, Property>) -> Property {
-        return base[keyPath: keyPath]
+public extension SafeGen {
+    /// Reverts back to a standard `Gen`
+    var unsafe: Gen<T> {
+        base
     }
 }
 
 public extension Gen {
-    /// Namespacing for safe static extension `Gen`
-    static var safe: SafeGen<T>.Type {
-        get { SafeGen<T>.self }
-    }
-    /// Namespacing for safe extension functions for `Gen`
+    /// Converts the `Gen` to the `SafeGen` wrapper
     var safe: SafeGen<T> {
-        get { SafeGen(self) }
+        SafeGen(self)
     }
 }
