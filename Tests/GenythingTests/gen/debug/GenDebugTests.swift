27e895e15b45f8208da12ad4fca183cdc19d6662
Nico Richard <nicorichard@gmail.com>
2022-03-01 14:23:17 -0600

    Add also & debug tests

---
+++ Tests/GenythingTests/gen/debug/GenDebugTests.swift
@@ -0,0 +1,13 @@
+import XCTest
+import Genything
+import GenythingTest
+
+class Gen_DebugTests: XCTestCase {
+    func test_debug_printsAsExpected() {
+        _ = Gen.from(1...5)
+            .debug()
+            .map { $0 * 2 }
+            .debug("*2")
+            .take(5)
+    }
+}
