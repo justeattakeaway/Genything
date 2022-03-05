275255465906493a4d5ce7a319ce93a468a36c0b
Nico Richard <nicorichard@gmail.com>
2022-03-05 16:31:51 -0600

    Additions: "Also" for side-effects and "debug" for printing debug messages (#33)
    * Add a function for performing side-effects and a debug function like rxswift
    
    * Add also & debug tests

---
+++ Tests/GenythingTests/gen/debug/GenAlsoTests.swift
@@ -0,0 +1,16 @@
+import XCTest
+import Genything
+import GenythingTest
+
+class Gen_AlsoTests: XCTestCase {
+    func test_also_runs_side_effect_once_per_generation() {
+        let iterations = 100
+
+        var count = 0
+        _ = Gen.constant(()).also {
+            count += 1
+        }.take(iterations)
+
+        XCTAssertEqual(iterations, count)
+    }
+}
