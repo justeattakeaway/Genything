3ab8ad5f29f48895ae640d5e72a557164dbfef11
Nico Richard <nicorichard@gmail.com>
2022-03-06 12:51:11 -0600

    Add a test to show a deck of cards metaphor

--- Tests/GenythingTests/gen/build/GenShuffledTests.swift
+++ Tests/GenythingTests/gen/build/GenShuffledTests.swift
@@ -36,4 +36,30 @@ final internal class GenShuffledTests: XCTestCase {
             history.append($0)
         }
     }
+
+    func test_shuffledDistribution_deckOfCardsMetaphor() {
+        enum Suit: CaseIterable {
+            case heart, spade, club, diamond
+        }
+
+        enum Rank: CaseIterable {
+            case ace, two, three, four, five, six, seven, eight, nine, ten, jack, queen, king
+        }
+
+        struct Card: Equatable, Hashable {
+            var suit: Suit
+            var rank: Rank
+        }
+
+        let cardGen = Gen<Card>.shuffledDistribution(
+            Rank.allCases.map { rank in Suit.allCases.map { suit in Card(suit: suit, rank: rank) }}
+                .flatMap { $0 }
+        )
+
+        // All cards of the deck are drawn, and they are all unique
+        XCTAssertEqual(52, Set<Card>(cardGen.take(52)).count)
+
+        // Drawing more from the deck takes from a newly shuffled pile and will repeat values
+        XCTAssertEqual(52, Set<Card>(cardGen.take(53)).count)
+    }
 }
