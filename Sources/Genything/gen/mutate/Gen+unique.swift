36f72446d24f4f226468e25e5ec49375f7727583
Nico Richard <nicorichard@gmail.com>
2022-03-09 11:51:30 -0600

    Remove unused object

--- Sources/Genything/gen/mutate/Gen+unique.swift
+++ Sources/Genything/gen/mutate/Gen+unique.swift
@@ -2,27 +2,6 @@ import Foundation
 
 // MARK: Mutate
 
-/// A cache capable of storing the unique values created by a particular Generator's id for the lifetime of the `Context`
-///
-/// - SeeAlso: `Gen.unique()`
-private class UniqueStorage {
-    private var data: [UUID:[Any]] = [:]
-
-    func contains<T: Equatable>(_ value: T, id: UUID) -> Bool {
-        data[id]?.contains {
-            ($0 as? T) == value
-        } ?? false
-    }
-
-    func store<T: Equatable>(_ value: T, id: UUID) {
-        if let existing = data[id] {
-            data[id] = existing + [value]
-        } else {
-            data[id] = [value]
-        }
-    }
-}
-
 public extension Gen where T: Equatable {
 
     /// Returns: A generator that only produces unique values
