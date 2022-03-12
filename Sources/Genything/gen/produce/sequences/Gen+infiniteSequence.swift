63536c965ad539e3ef653033667bac14b7ebd64c
Nico Richard <nicorichard@gmail.com>
2022-03-12 09:38:00 -0600

    Add documentation for sequence creators

--- Sources/Genything/gen/produce/sequences/Gen+infiniteSequence.swift
+++ Sources/Genything/gen/produce/sequences/Gen+infiniteSequence.swift
@@ -2,7 +2,15 @@ import Foundation
 
 // MARK: Produce
 
-public extension Gen {
+extension Gen {
+    /// Returns: An infinite sequence of this generator's values
+    ///
+    /// - Warning: The resulting sequence is infinite
+    ///
+    /// - Parameters:
+    ///   - context: The context to be used for generation
+    ///
+    /// - Returns: The Sequence
     func infiniteSequence(context: Context = .default) -> InfiniteSequence<T> {
         InfiniteSequence {
             generate(context: context)
