70d37145999b72f469d3e0690f8fee4d91346991
Nico Richard <nicorichard@gmail.com>
2022-03-23 09:11:16 -0500

    Fixing the documentation generator and beginning to fill in documentation we are missing

--- Gemfile
+++ Gemfile
@@ -1,4 +1,4 @@
 source 'https://rubygems.org'
 
 gem 'cocoapods', '1.10.1'
-gem 'jazzy'
+gem 'jazzy', '0.14.2'
--- Sources/Genything/Arbitrary/Instance/CaseIterable+arbitrary.swift
+++ Sources/Genything/Arbitrary/Instance/CaseIterable+arbitrary.swift
@@ -1,4 +1,9 @@
 extension CaseIterable {
+    /// Returns: A generator of arbitrary cases
+    ///
+    /// - Precondition: The receiver's `allCases` collcetion must not be empty
+    ///
+    /// - Returns: A generator of the receiver's arbitrary cases
     public static var arbitrary: AnyGenerator<AllCases.Element> {
         let cases = allCases
         assert(!cases.isEmpty, "`CaseIterable.arbitrary` called without cases.")
