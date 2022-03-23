d66dde8ad61b41af13f9584f07fe04987719e272
Nico Richard <nicorichard@gmail.com>
2022-03-23 13:39:11 -0500

    improve comment

--- Sources/Genything/Producers/Generator+take.swift
+++ Sources/Genything/Producers/Generator+take.swift
@@ -4,7 +4,7 @@ extension Generator {
     /// Returns: An array of values from the generator
     ///
     /// - Parameters:
-    ///   - count: The amount of values to generate, default's to the randomSource's `iterations` value
+    ///   - count: The amount of values to generate
     ///   - randomSource: The randomSource to be used for generation
     ///
     /// - Returns: An array of generated values
