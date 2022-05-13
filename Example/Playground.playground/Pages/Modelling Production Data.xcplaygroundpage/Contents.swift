7bd950fe4bd511470ed3c7866f2a62fd4cbad340
nicorichard <nicorichard@gmail.com>
2022-05-13 16:02:18 -0500

    improve modelling production data playground

--- Example/Playground.playground/Pages/Modelling Production Data.xcplaygroundpage/Contents.swift
+++ Example/Playground.playground/Pages/Modelling Production Data.xcplaygroundpage/Contents.swift
@@ -4,49 +4,51 @@ import GenythingTest
 import Trickery
 import XCTest
 
-// Before this, we've seen how Genything can be used to generate mostly numbers. But we're developing real apps, not doing equations!
-// Let's take some example models that more closely resemble how apps are made:
+/*:
+ Before this, we've seen how Genything can be used to generate mostly numbers. But we're developing real apps, not doing equations!
 
-enum Handedness { case left, right, ambi }
-
+ Let's take a look at a model that more closely resembles something from an app:
+*/
 struct User {
     var id: UUID
     var name: String
     var age: Int
-    var handedness: Handedness
+    
+    enum Handedness {
+        case left, right, ambidextrous
+    }
 }
 
-// Genything is more than happy to be used to model more complex data types, you can compose it's generators to fill each of the object's properties.
 
-let userGenerator = AnyGenerator { rs in
-    User(
-        id: UUID.arbitrary.next(rs),
-        name: String.arbitrary.next(rs),
-        age: Int.arbitrary.next(rs),
-        handedness: [Handedness.left, .right, .ambi].arbitrary.next(rs)
-    )
-}
 
-userGenerator.next(.predetermined()) /// Returns: An arbitrary user
 
-// As you can see it is not difficult to create a generator which is capable of quickly providing us with arbitrary users of our system. But we can do even better!
-
-// 1. By adopting CaseIterable, we can auto-synthesize arbitrary conformance for Handedness
-// 2. By trusting Swift's type inferance engine, we can do some cleaning up
-
-extension Handedness: CaseIterable {}
-
+//: We can build a generator for this model using the AnyGenerator factory
 AnyGenerator { rs in
     User(
-        id: .arbitrary.next(rs),
-        name: .arbitrary.next(rs),
+        id: UUID.arbitrary.next(rs),
+        name: .arbitrary.next(rs), /// We can even omit the type and let Swift infer it
         age: .arbitrary.next(rs),
         handedness: .arbitrary.next(rs)
     )
 }
 
-// 🎉 Much better! Genything even offers a composer which takes this one step further (but is by no means required knowledge)
 
+
+
+//: By adopting CaseIterable, we can auto-synthesize arbitrary conformance for Handedness
+extension Handedness: CaseIterable {}
+
+
+
+
+
+
+
+
+
+
+
+//: Genything also offers a composer which simplifies the factory by automatically searching for arbitrary conformance and performing the generation
 Generators.compose { generate in
     User(
         id: generate(),
@@ -56,8 +58,17 @@ Generators.compose { generate in
     )
 }
 
-// By conforming your type to `Arbitrary` you can provide this common generator for anyone wishing to build an Example / Preview / Test which depends on this model.
 
+
+
+
+
+
+
+
+
+
+//: By conforming your type to Arbitrary you can make the User available to any subsequent generators or composers
 extension User: Arbitrary {
     static var arbitrary: AnyGenerator<User> {
         Generators.compose { generate in
@@ -70,56 +81,86 @@ extension User: Arbitrary {
         }
     }
 }
+//: 🕵️ If you noticed that this may be a great candidate for code generation, you would be right. Genything offers Sourcery templates to automatically create arbitrary generators for all of your models.
 
-// 🕵️ If you've noticed that every line is the same, and that this is a great candidate for code generation, you would be right. Genything offers Sourcery templates to automatically create arbitrary generators for all of your models.
 
-// ---
 
-// 🙋 > "But this generator is really... arbitrary! I have specific requirements for my Example / Preview / Test."
 
-// We recommend that teams first create arbitrary representations of their models, but then adapt them to the situation at hand.
 
-// For example, if you are required to test a routine that only occurs if the user is an adult:
 
-extension User {
-    var isAdult: Bool { age >= 18 }
+
+
+/*:
+ Let's take a look at a practical example. Consider some function `isAdult(user:)`, which we want to write some unit tests to verify.
+*/
+func isAdult(_ user: User) -> Bool {
+    user.age >= 18
 }
 
-// You may want a random space from which to draw Users that are **over** 18, and another from which to draw users that are **below** 18
-
-// This can be done by regenerating (or recomposing) the data type
-
-let over18UserGen = User.arbitrary.regenerate { user, randomSource in
-    user.age = (18...Int.max).arbitrary.next(randomSource)
+/*:
+ A traditional pattern to test this might look something like the following.
+ 
+ It's up to the developer to choose adequate values for the test, including edge cases.
+ Models can grow to be quite large, and it can become difficult to initialize all of the properties with dummy data.
+ 
+ We also are not likely to test different properties, we can't say that "an 18 year olf user with **any name** is an adult".
+ */
+class TestAgeTraditional: XCTestCase {
+    func makeSut(age: Int) -> User {
+        User(id: UUID(), name: "", age: age)
+    }
+    
+    func test_givenUnder18Users_isAdult_isFalse_forAChild() {
+        var sut = makeSut(age: 0)
+        XCTAssertFalse(isAdult(sut))
+        sut.age = 17
+        XCTAssertFalse(isAdult(sut))
+    }
+    
+    func test_givenUnder18Users_isAdult_isFalse_forEdgeCases() {
+        var sut = makeSut(age: -1)
+        XCTAssertFalse(isAdult(sut))
+        sut.age = Int.min
+        XCTAssertFalse(isAdult(sut))
+    }
 }
 
-over18UserGen.next(.predetermined()) /// Returns: An arbitrary user with age >= 18
+TestAgeTraditional.defaultTestSuite.run()
 
-let under18UserGen = User.arbitrary.regenerate { user, randomSource in
-    user.age = (0..<18).arbitrary.next(randomSource)
-}
+/*:
+ Using Genything we can test the User for invariants. By leaving the properties that are meant to be irrelevant to the  test as arbitrary and conducting multiple tests, we can be reasonably sure of this assumption.
+ */
 
-under18UserGen.next(.predetermined()) /// Returns: An arbitrary user with age < 18
-
-// We assume that the routine should be invariant to any other User property other than the age
-// By leaving the other properties arbitrary, and conducting multiple tests, we can be reasonably sure of this assumption
-
-class TestAgeRouting: XCTestCase {
+class TestAgeUsingGenything: XCTestCase {
+    // We can regenerate the user generator we created previously
+    let under18UserGen = User.arbitrary.regenerate { user, randomSource in
+        user.age = (0..<18).arbitrary.next(randomSource) // and we can adapt it to only return users <18 years old
+    }
+    // Then using GenythingTest we can use it's test functions
     func test_givenUnder18Users_isAdult_returnsFalse() {
+        // for all users where age is 0..<18
         testAllSatisfy(under18UserGen) { user in
-            !user.isAdult
+            // the following condition should hold
+            !isAdult(user)
         }
     }
 }
 
 TestAgeRouting.defaultTestSuite.run()
 
-// ---
 
-// 🧑‍🎨 > "This data is still very arbitrary, how can I generate nice previews and example apps"
 
-// In this case we can reach for `Trickery`, a companion library of **realistic** generators.
 
+
+
+
+
+
+
+/*:
+ 🧑‍🎨 > "This data is still very arbitrary, how can I generate nice previews and example apps"
+ In this case we can reach for `Trickery`, a companion library of **realistic** generators.
+ */
 User.arbitrary.recompose { user, generate in
     user.name = generate(Fake.PersonNames.name)
     user.age = generate((0...100).arbitrary)
